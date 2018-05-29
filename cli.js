1988e5df7fd7e49bd8f43e369b12432e25f4ac2b
Toyobayashi <lifenglin314@outlook.com>
2018-05-29 23:42:17 +0800

    1.0.2

--- cli.js
+++ cli.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
 const extractACB = require('.')
-extractACB(...process.argv.slice(2))
+const argv = process.argv.slice(2)
+if (argv.length < 1) {
+  console.log('\n  Usage:\n')
+  console.log('    acb path/to/somefile.acb [outputdir]\n')
+} else {
+  extractACB(...argv)
+}
